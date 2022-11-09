bcf0c42d8f987956c0833566787720ba162c845a
Daniel Szlaski <daniel.szlaski@genesys.com>
2022-11-09 12:48:17 +0100

    include version

--- template/node16/function/handler.js
+++ template/node16/function/handler.js
@@ -1,8 +1,10 @@
 "use strict";
 const utils = require("./libs/utils.js");
 const responseLib = require("./libs/response-lib");
+const pkg = require("./package.json");
 
 module.exports = async (event, context) => {
+  console.log(`> ${pkg.name}@${pkg.version}`);
   // Token Verification
   // const verified = await utils.verifyToken(event);
   // if (!verified) {
